b034ddbd48dd165fb43c3902a9a62af58bbd03b3
wht <wanght@shenghesun.com>
2018-10-15 14:59:25 +0800

    update

--- pages/detail/showDetail.js
+++ pages/detail/showDetail.js
@@ -3,88 +3,108 @@ const app = getApp()
 var config = require('../../utils/config.js');
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    price:'',
-    classtype:'',
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        price: '',
+        classtype: '',
+        tabs: ["国内", "国际"],
+        activeIndex: 0,
+        sliderOffset: 0,
+        sliderLeft: 128
+    },
+    onLoad: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            }
+        });
+    },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    },
+    //跳转至下保单页面
+    to_insured: function (e) {
+        if (e.detail.errMsg == "getUserInfo:ok") {
+            if (!app.globalData.userInfo || !wx.getStorageSync('userId')) {
+                console.log(app.globalData.userInfo);
+                //获取用户数据
+                app.login();
+            }
+            wx.navigateTo({
+                url: '/pages/order/writeOrder?price=' + this.data.price + '&classtype=' + this.data.classtype
+            })
+        }
+    },
 
-  //跳转至下保单页面
-  to_insured: function (e) { 
-    if (e.detail.errMsg == "getUserInfo:ok") {
-      if (!app.globalData.userInfo || !wx.getStorageSync('userId')) {
-       console.log(app.globalData.userInfo);
-        //获取用户数据
-        app.login();
-      }
-      wx.navigateTo({
-        url: '/pages/order/writeOrder?price=' + this.data.price + '&classtype=' + this.data.classtype
-      })
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that = this;
+        that.setData({
+            price: options.price,
+        }),
+            that.setData({
+                classtype: options.classtype,
+            })
+        console.log(that.data.classtype);
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-    that.setData({
-      price: options.price,
-    }),
-      that.setData({
-        classtype: options.classtype,
-      })
-      console.log(that.data.classtype);
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,100 +3,111 @@
 const app = getApp()
 var config = require('../../utils/config.js');
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    pageStyle: `width:${app.globalData.width};height:${app.globalData.height}`,
-    scale: app.globalData.windowWidth / app.globalData.windowHeight,
-    foreignPrice:'2000',
-    foreignType:'2',
-    innerPrice:'1000',
-    innerType:'1',
-    //登机牌信息
-    passResult:'',
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.redirectTo({
-      url: '/pages/logs/logs'
-    })
-  },
-  //跳转至保险详情说明页面
-  to_detail_inner: function (e) {
-    wx.navigateTo({
-      url: '/pages/detail/showDetail?price=' + this.data.innerPrice+'&classtype='+this.data.innerType
-    })
-  },
-  to_detail_foreign: function (e) {
-    wx.navigateTo({
-      url: '/pages/detail/showDetail?price=' + this.data.foreignPrice + '&classtype=' + this.data.foreignType
-    })
-  },
-  //扫描
-  scan: function () {
-    var that = this;
-    console.log("进入扫描");
-    wx.scanCode({
-      scanType: ['qrCode', 'barCode', 'datamatrix','pdf417'],
-     success: function(res){
-       that.setData({
-         passResult: res.result,
-       })
-       console.log(that.data.passResult);
-       var dateList = that.data.passResult.split(" ");
-       var arr = []
-       for (var i in dateList) {
-         if (dateList[i].length!=0){
-           arr = arr.concat(dateList[i]);
-           console.log(arr);
-      }
-       }
-       var flight = arr[2] + arr[3];
-       var startCity = arr[2].substring(0, 3);
-       var endCity = arr[2].substring(3,6);
-       var flightNo = flight.substring(6, 12);
-     }
-    })
-  },
-
-  onLoad: function () {
-    console.log(app.globalData)
-    console.log(this.data.canIUse)
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        pageStyle: `width:${app.globalData.width};height:${app.globalData.height}`,
+        scale: app.globalData.windowWidth / app.globalData.windowHeight,
+        foreignPrice: '2000',
+        foreignType: '2',
+        innerPrice: '1000',
+        innerType: '1',
+        //登机牌信息
+        passResult: '',
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.redirectTo({
+            url: '/pages/logs/logs'
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    },
+    //跳转至保险详情说明页面
+    to_detail_inner: function (e) {
+        wx.showModal({
+            title: '正在建设中，敬请期待...',
+            content: '功能正在建设中，敬请期待...',
+            showCancel: false,
+            // success: function (res) {
+            //     if (res.confirm) {
+            //         console.log('用户点击确定')
+            //     }
+            // }
+        });
+
+        // wx.navigateTo({
+        //   url: '/pages/detail/showDetail?price=' + this.data.innerPrice+'&classtype='+this.data.innerType
+        // })
+    },
+    to_detail_foreign: function (e) {
+        wx.navigateTo({
+            url: '/pages/detail/showDetail?price=' + this.data.foreignPrice + '&classtype=' + this.data.foreignType
+        })
+    },
+    //扫描
+    scan: function () {
+        var that = this;
+        console.log("进入扫描");
+        wx.scanCode({
+            scanType: ['qrCode', 'barCode', 'datamatrix', 'pdf417'],
+            success: function (res) {
+                that.setData({
+                    passResult: res.result,
+                })
+                console.log(that.data.passResult);
+                var dateList = that.data.passResult.split(" ");
+                var arr = []
+                for (var i in dateList) {
+                    if (dateList[i].length != 0) {
+                        arr = arr.concat(dateList[i]);
+                        console.log(arr);
+                    }
+                }
+                var flight = arr[2] + arr[3];
+                var startCity = arr[2].substring(0, 3);
+                var endCity = arr[2].substring(3, 6);
+                var flightNo = flight.substring(6, 12);
+            }
+        })
+    },
+
+    onLoad: function () {
+        console.log(app.globalData)
+        console.log(this.data.canIUse)
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
         }
-      })
+    },
+    getUserInfo: function (e) {
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
     }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
