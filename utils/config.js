9decde2660324d93f4089320f63c306d8dd71963
yangzp <yangzp@shenghesun.com>
2018-10-19 09:41:43 +0800

    uupdate

--- utils/config.js
+++ utils/config.js
@@ -2,15 +2,12 @@
  * 小程序配置文件
  */
 //接口域名
-//var host = "http://127.0.0.1:4455";
-var host = "https://tennismanage.dazonghetong.com";
+var host = "http://127.0.0.1:4455";
+//var host = "https://tennismanage.dazonghetong.com";
 
 var config = {
   host,
-  baseUrl: `${host}`,
-//   图片背景
-  bg_img: `${host}/f/backgroundimage`,
-  onShareAppMessageTitle :["我歌技了得，敢不敢一起合唱！", "歌已点好，快来与我合唱！"],
+  baseUrl: `${host}`
 };
 
 module.exports = config
